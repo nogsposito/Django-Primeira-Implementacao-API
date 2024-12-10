9bae9bcf719e4ae1adb089de0f5dd8b7c7ef6ba8
nogsposito <vsan@cesar.school>
2024-12-09 23:32:11 -0300

    basico completo

--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,7 +1,23 @@
+import requests
+
 from django.shortcuts import render
 from django.views import View
 
 class MainHomeView(View):
     
     def get(self, request):
-        return render(request, 'main.html')
+        url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata'
+        response = requests.get(url)
+
+        if response.status_code == 200:
+
+            dados = response.json()
+            receita = dados['meals'][0]
+
+            context = {
+                'nome': receita['strMeal'],
+                'categoria': receita['strCategory'],
+                'instrucoes': receita['strInstructions'],
+            }
+
+        return render(request, 'main.html', context)
